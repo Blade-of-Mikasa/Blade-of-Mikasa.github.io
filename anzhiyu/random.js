84c15a8a07053840cef0aef71c34391ad767227a
1273710251@qq.com <1273710251@qq.com>
2025-03-06 23:00:48 +0800

    Site updated: 2025-03-06 23:00:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/06/DP-专题/","2025/01/16/Edu-Round-141/","2024/12/28/Round-941-div4/","2024/12/28/Round-944-div4/","2024/12/28/Round-947-div3/","2024/12/28/Round-988-div3/","2025/03/06/div3-762/","2024/12/28/欢迎/"];function toRandomPost(){
+var posts=["2025/03/06/DP-专题/","2025/01/16/Edu-Round-141/","2024/12/28/Round-944-div4/","2024/12/28/Round-941-div4/","2024/12/28/Round-947-div3/","2024/12/28/欢迎/","2024/12/28/Round-988-div3/","2025/03/06/div3-762/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
