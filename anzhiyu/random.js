04a84b2e8fb46c708ae43ba29f7d1f3c74489134
Blade-of-Mikasa <1273710251@qq.com>
2026-02-12 05:13:49 +0800

    Site updated: 2026-02-12 05:13:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/09/Edu板刷记录/","2026/02/12/mit-6-1810-2-system-calls/","2025/11/25/关于本博客/","2026/02/09/mit-6-1810-1-Xv6-and-Unix-utilities/","2026/01/06/巧思题随记/"];function toRandomPost(){
+var posts=["2025/12/09/Edu板刷记录/","2026/01/06/巧思题随记/","2026/02/09/mit-6-1810-1-Xv6-and-Unix-utilities/","2026/02/12/mit-6-1810-2-system-calls/","2025/11/25/关于本博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
